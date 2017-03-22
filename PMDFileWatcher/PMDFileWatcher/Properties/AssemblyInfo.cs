0ab27a92944650aa4726f5135e753f8d4380a94e
rerrahkr <rerrah57@gmail.com>
2017-03-22 21:35:49 +0900

    バージョン情報修正

--- PMDFileWatcher/PMDFileWatcher/Properties/AssemblyInfo.cs
+++ PMDFileWatcher/PMDFileWatcher/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("PMDMML監視・自動コンパイル・自動再生補助ツール")]
-[assembly: AssemblyDescription("指定したPMDのMMLを監視、変更があれば自動コンパイル&再生")]
+[assembly: AssemblyTitle("PMDFileWatcher")]
+[assembly: AssemblyDescription("PMDMML監視・自動コンパイル・自動再生補助ツール")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PMDFileWatcher")]
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
 [assembly: NeutralResourcesLanguage("ja-JP")]
 
