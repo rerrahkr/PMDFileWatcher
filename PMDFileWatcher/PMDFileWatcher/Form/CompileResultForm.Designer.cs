850955515f78173a593175828da73472b71b7810
rerrahkr <rerrah57@gmail.com>
2017-10-03 23:46:20 +0900

    アイコンを設定

--- PMDFileWatcher/PMDFileWatcher/Form/CompileResultForm.Designer.cs
+++ PMDFileWatcher/PMDFileWatcher/Form/CompileResultForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CompileResultForm));
             this.resultTextBox = new System.Windows.Forms.TextBox();
             this.okButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -64,6 +65,7 @@
             this.Controls.Add(this.okButton);
             this.Controls.Add(this.resultTextBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "CompileResultForm";
             this.Text = "コンパイル結果";
