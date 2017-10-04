3155226ddd10969eb09c663f20234080b2f4b86f
rerrahkr <rkr.5.tecknzi@gmail.com>
2017-10-04 18:12:48 +0900

    バージョン情報フォームにアイコン表示

--- PMDFileWatcher/PMDFileWatcher/Form/VersionForm.cs
+++ PMDFileWatcher/PMDFileWatcher/Form/VersionForm.cs
@@ -23,6 +23,10 @@ namespace PMDFileWatcher.Form
                     System.Reflection.Assembly.GetExecutingAssembly(),
                     typeof(System.Reflection.AssemblyCopyrightAttribute));
             copyrightLabel.Text = aca.Copyright;
+            string path = System.Reflection.Assembly.GetEntryAssembly().Location;
+            iconPictureBox.Image = Icon.ExtractAssociatedIcon(path).ToBitmap();
+
+            // TODO: 設定保存場所変更
         }
 
         private void okButton_Click(object sender, EventArgs e)
